80ccaadbf66837530ba654db45103d827d30f336
Tisdale511 <tisdalefry@gmail.com>
2020-07-16 12:24:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    textReplace()
+});
+
+function textReplace(){
+    let container = document.getElementById('text')
+    container.textContent = "This is really cool!"
+
+}
